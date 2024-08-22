28249a9297eb39203eb1b6ffb332f9fda675fa75
ArraAkshaya <149825952+ArraAkshaya@users.noreply.github.com>
2024-08-22 16:19:34 +0000

    conversions done

---
+++ 01_basics/conversion.js
@@ -0,0 +1,25 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+
+//dataytpes
+//primitive - String, Number, Boolean, null, undefined, Symbol, BigInt
+
+//reference type or non primitive type:
+//Array, Objects, Functions
+
+const heroes = ["Captain America", "Spiderman", "Ironman", "doctor strange"];
+let myObj = {
+    String: "Akshaya",
+    age: 20
+}
+
+const myFunction = function(){
+    console.log("Hi!");
+}
+
+
